ea8218c4306fe176d7ac447e3221b9c724df2cd3
PLeepl <3283962220@qq.com>
2022-05-06 16:18:38 +0800

    updata

---
+++ JD.COM_DEV_ING/js/js_register/register_Pleepl.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    // 手机号验证
+    //1.获取元素 =表示赋值  
+    var phone = document.querySelector('#phone');
+    var sjts = document.querySelector('#sjts');
+    var wzts1 = document.querySelector('#wzts1');
+    // 2.设置手机号验证规则-正则表达式(/^开始 $/结束)
+    var gz1 = /^[1][3-9][0-9]{9}$/
+        // {9}只管[0-9]
+
+
+
+
+
+}
