744bf5ccca46f3e6ff5b6db3f2be717dfede0e7e
PLeepl <3283962220@qq.com>
2022-05-13 12:22:55 +0800

    updata

--- JD.COM_DEV_ING/js/js_register/register_Pleepl.js
+++ JD.COM_DEV_ING/js/js_register/register_Pleepl.js
@@ -4,6 +4,7 @@ window.onload = function() {
     var phone = document.querySelector('#phone');
     var sjts = document.querySelector('#sjts');
     var wzts1 = document.querySelector('#wzts1');
+    // querySelector 获取
     // 2.设置手机号验证规则-正则表达式(/^开始 $/结束)
     var gz1 = /^[1][3-9][0-9]{9}$/;
     // {9}只管[0-9]
@@ -34,4 +35,53 @@ window.onload = function() {
 
 
     })
+
+
+    //  邮箱验证
+    //1.获取元素 =表示赋值 右边给左边
+    var Emails = document.querySelector('#Emails');
+    var yxts = document.querySelector('#yxts');
+    var wzts2 = document.querySelector('#wzts2');
+    // querySelector 获取
+    // 2.设置邮箱验证规则-正则表达式(/^开始 $/结束)
+    var gz2 = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+    // {9}只管[0-9]
+    // 3.邮箱文本框获取焦点或失去焦点时的提示事件
+    Emails.addEventListener('focus', function() {
+            if (this.value == '') {
+                yxts.style.display = 'block';
+                wzts2.innerText = '请输入邮箱号！';
+            } else {
+                yxts.style.display = 'none';
+            }
+
+
+        })
+        // focus 获得焦点
+    Emails.addEventListener('blur', function() {
+        // sjts.style.display = 'none';
+        if (this.value == '') {
+            wzts2.innerText = '邮箱不能为空！';
+        } else {
+            if (gz2.test(this.value) === true) {
+                yxts.style.display = 'none';
+            } else {
+                yxts.style.display = 'block';
+                wzts2.innerText = '邮箱号格式错误！';
+            }
+        }
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
 }
