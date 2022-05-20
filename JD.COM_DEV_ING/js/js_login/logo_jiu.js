7b8ed61a2b271b1ec489fe0ce17a03f284c2ffb4
pujiurong <102287710+pujiurong@users.noreply.github.com>
2022-05-20 16:34:28 +0800

    update
    更新原有文件

---
+++ JD.COM_DEV_ING/js/js_login/logo_jiu.js
@@ -0,0 +1,42 @@
+window.onload = function() {
+    var smdl = document.querySelector('.login-tab-l');
+    var zhdl = document.querySelector('.login-tab-r');
+    var sm = document.querySelector('.qrcode-login');
+    var zh = document.querySelector('.login-box');
+    var aaa = document.querySelector('#aaa');
+    var loginbtn = document.querySelector('.login-btn');
+    var loginname = document.querySelector('#loginname');
+    var loginpwd = document.querySelector('#nloginpwd');
+    var reg1 = /^[1][3456789][0-9]{9}$/;
+    var reg2 = /^\w{8,20}$/;
+    smdl.addEventListener('click', function() {
+        sm.style.display = 'block';
+        zh.style.display = 'none';
+    })
+    zhdl.addEventListener('click', function() {
+        zh.style.display = 'block';
+        sm.style.display = 'none';
+    })
+    sm.addEventListener('mouseenter', function() {
+        aaa.style.display = 'block';
+    })
+    sm.addEventListener('mouseleave', function() {
+        aaa.style.display = 'none';
+    })
+    loginbtn.addEventListener('click', function() {
+        if (loginname.value == '' || loginpwd.value == '') {
+            alert('账户和密码不能为空');
+        } else {
+            if (reg1.test(loginname.value) == false || reg2.test(loginpwd.value) == false) {
+                alert('账户和密码格式错误');
+            } else {
+                if (loginname.value == '17713563382' && loginpwd.value == '12345678') {
+                    alert('登录成功!');
+                    window.location.href = 'index_jiu.html';
+                } else {
+                    alert('账户和密码错误');
+                }
+            }
+        }
+    })
+}
