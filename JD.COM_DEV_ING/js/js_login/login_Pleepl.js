3b275dd3e55bf448493e85a07fcac59f4106445e
PLeepl <3283962220@qq.com>
2022-05-20 15:05:38 +0800

    update

--- JD.COM_DEV_ING/js/js_login/login_Pleepl.js
+++ JD.COM_DEV_ING/js/js_login/login_Pleepl.js
@@ -10,15 +10,51 @@ window.onload = function() {
         zh.style.diasplay = 'none';
         mc.style.display = 'none';
         smdl.style.color = 'red';
+        smdl.style.fontweight = 'bold';
         zhdl.style.color = '#666';
     })
 
     zhdl.addEventListener('click', function() {
-        sm.style.display = 'none';
-        zh.style.diasplay = 'block';
-        mc.style.display = 'block';
-        zhdl.style.color = 'red';
-        smdl.style.color = '#666';
+            sm.style.display = 'none';
+            zh.style.diasplay = 'block';
+            mc.style.display = 'block';
+            zhdl.style.color = 'red';
+            smdl.style.color = '#666';
+        })
+        // 账户验证
+    var loginbtn = document.querySelector('.login-btn');
+    var loginname = document.querySelector('#loginname');
+    var loginpwd = document.querySelector('#loginpwd');
+
+
+    var gz1 = /^[1][3-9][0-9]{9}$/;
+    var gz2 = /^(?=.*[a-zA-Z])(?=.*\d).{8,20}$/;
+    loginbtn.addEventListener('click', function() {
+        if (loginname.valve == '' || loginpwd.value == '') {
+            alert('账号或密码不能为空！');
+        } else {
+            if (gz1.test(loginname.value) == false || gz2.test(loginpwd.value) == false) {
+                alert('账号或密码格式错误！');
+            } else {
+                if (loginname.value == '14780032117' && loginpwd.value == "12345678pl") {
+                    alert('登录成功！');
+                    window.location.href = '../index_Pleepl.html';
+                } else {
+                    alert('用户不存在！');
+                }
+            }
+        }
+
+
+
+
     })
+
+
+
+
+
+
+
 
 }
