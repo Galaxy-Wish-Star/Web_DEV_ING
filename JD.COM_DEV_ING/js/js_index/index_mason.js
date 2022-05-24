43fd803cdcd88dfbb3f4263be6e9ae0b7c0fd1c8
mason369 <maosenyang00@gmail.com>
2022-05-24 10:14:17 +0800

    Update index_mason.js

--- JD.COM_DEV_ING/js/js_index/index_mason.js
+++ JD.COM_DEV_ING/js/js_index/index_mason.js
@@ -1,10 +1,15 @@
 $(function () {
-    // var menu = document.querySelector('.top .menu')
+    // var menu = document.querySelector('.top .menu')顶部菜单
     $('.top .menu').mouseenter(function () {
         $(this).children('.dd').show();
     })
     $('.top .menu').mouseleave(function () {
         $(this).children('.dd').hide();
     })
+
+    $('.header .banner .one ul li').mouseleave(function (){
+        
+    })
+
 
 })
