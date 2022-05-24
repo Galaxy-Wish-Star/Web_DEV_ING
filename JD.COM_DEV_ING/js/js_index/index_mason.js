025bd6753ed753e9ccfa48b3a3192805fad3b757
mason369 <maosenyang00@gmail.com>
2022-05-24 11:36:42 +0800

    Update index_mason.js

--- JD.COM_DEV_ING/js/js_index/index_mason.js
+++ JD.COM_DEV_ING/js/js_index/index_mason.js
@@ -7,8 +7,21 @@ $(function () {
         $(this).children('.dd').hide();
     })
 
-    $('.header .banner .one ul li').mouseleave(function (){
-        
+    $('.header .banner .one ul li').mouseenter(function() {
+        $('#J_popCtn').show();
+        $('#J_popCtn .cate_part').eq($(this).index()).show();
+    })
+    $('.header .banner .one ul li').mouseleave(function() {
+        $('#J_popCtn').hide();
+        $('#J_popCtn .cate_part').eq($(this).index()).hide();
+    })
+    $('#J_popCtn .cate_part').mouseenter(function() {
+        $('#J_popCtn').show();
+        $('#J_popCtn .cate_part').eq($(this).index()).show();
+    })
+    $('#J_popCtn .cate_part').mouseleave(function() {
+        $('#J_popCtn').hide();
+        $('#J_popCtn .cate_part').eq($(this).index()).hide();
     })
 
 
