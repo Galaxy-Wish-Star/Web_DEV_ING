fd0ffa69672d788caabf37d6a34929935ae660b9
PLeepl <3283962220@qq.com>
2022-06-06 17:28:47 +0800

    Revert "updata"
    This reverts commit 577c07b396f43f8970122eebc4bf6db49a65c1a9.

--- JD.COM_DEV_ING/js/js_index/index_Pleep.js
+++ JD.COM_DEV_ING/js/js_index/index_Pleep.js
@@ -27,31 +27,7 @@ $(function() {
         $('#J_popCtn .cate_part').eq($(this).index()).show();
     })
     $('#J_popCtn .cate_part').mouseleave(function() {
-            $('#J_popCtn').hide();
-            $('#J_popCtn .cate_part').eq($(this).index()).hide();
-        })
-        // 广告
-    $('.ggtc img').fadeIn(2000).delay(3000).animate({
-        opacity: '0',
-        width: '0'
-    }, 2000, function() {
-        $(this).hide();
-    })
-
-
-    // console.log($('.main inner one').offset().top);
-
-    //电梯导航
-    var elevator = $('.elevator'); //窗口滚动事件
-    $(window).scroll(function() {
-        // console.log($(document).scrollTop()); //鼠标滚动的高度
-        var h = $(document).scrollTop(); //h:卷去的头部
-        var e_h = $('.main .one').offset().top; //距离屏幕顶端的距离
-        console.log(e_h);
-        if (h > e_h) {
-            elevator.addClass('elevator_fix');
-        } else {
-            elevator.removeClass('elevator_fix');
-        }
+        $('#J_popCtn').hide();
+        $('#J_popCtn .cate_part').eq($(this).index()).hide();
     })
 })
