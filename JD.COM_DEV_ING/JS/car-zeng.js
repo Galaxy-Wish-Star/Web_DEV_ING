8f7dbbf079174fe610ec931104c23874d1a5f181
zenbing <3277168019@qq.com>
2022-06-09 10:33:38 +0800

    update
    更新了原文件的内容

--- JD.COM_DEV_ING/JS/car-zeng.js
+++ JD.COM_DEV_ING/JS/car-zeng.js
@@ -1,9 +1,15 @@
 $(function() {
     // 全选
     $('.checkall').change(function() {
-
+        // var isChecked=$(this).prop('checked');
+        // $('.j-checkbox').prop('.checked',isChecked);
+        $('.j-checkbox').prop('checked', $(this).prop('checked'));
     })
-
+    $('.j-checkbok').change(function() {
+        console.log($('.checkall').length);
+        console.log($('.j-checkbok').length);
+        console.log($('.j-checkbok:checked').length);
+    })
 
 
 
