29db83ec11a34948c7706dabb01dc65c06d96b0e
liusxs <1694181697@qq.com>
2022-10-05 18:34:37 +0800

    up

--- JD.COM_DEV_ING/js/js_index/index_liuliu.js
+++ JD.COM_DEV_ING/js/js_index/index_liuliu.js
@@ -26,5 +26,38 @@ $(function(){
     $('span').click(function(){
         $('.advert').remove()
     })
-    $('.advert').stop().slideDown(1000).fadeOut.dadeln(1000)
+    //电梯导航
+    var elevator = $('.elevator');
+    $(window).scroll(function() {
+     var h = $(document).scrollTop(); //卷去的头部
+     var e_h = $('.main .one').offset().top; //距离页面顶端的距离
+     // 将电梯导航固定到绝对位置
+    if (h > e_h) {
+        elevator.addClass('elevator_fix');
+    } else {
+        elevator.removeClass('elevator_fix');
+    }
+     // 滚动时电梯随之改变
+    $('.main .inner .leval').each(function(i, e) {
+        if (h >= $(e).offset().top) {
+            $('.elevator>ul>li').eq(i).addClass('active').siblings().removeClass('active')
+        } else {
+            ('.elevator>ul>li').eq(i).removeClass('active')
+        }
+    })
+    $('.elevator>ul>li').click(function() {
+        var current;
+        if ($(this).index() == 4) {
+        current = 0;
+        } else {
+            current = $('.main .inner .leval').eq($(this).index()).offset().top;
+        }
+        $('html').stop().animate({
+            scrollTop: current + 1
+        });
+        $(this).addClass('active').siblings().removeClass();
+    })
+})
+    // 广告方式
+    $('.advert').stop().slideDown(1000).fadeOut.dadeln(1000);
 })
--- JD.COM_DEV_ING/js/liuliu.car.js
+++ JD.COM_DEV_ING/js/liuliu.car.js
@@ -3,16 +3,17 @@ $(function(){
     var min_num=1;
     var max_num=10;
     $('.checkall').change(function(){
-        // var isChecked=$(this).prop('checked');
-        // $('.j-checkbox').prop('checked',isChecked)
+        var isChecked=$(this).prop('checked');
+        $('.j-checkbox').prop('checked',isChecked)
         $('.j-checkbox').prop('checked',$(this).prop('checked'));
-        // $('.checkall,j-checkbox').prop('checked',$(this).prop('checked'));
+        $('.checkall,j-checkbox').prop('checked',$(this).prop('checked'));
         // 全选中的商品增加背景色
         if($(this).prop('checked')){
             $('.cart-item').addClass('check-cart-item')
         }else{
             $('.cart-item').removeClass('check-cart-item')
         }
+        getSum();
     })
     // 被单选中的商品添加背景色
     $('.j-checkbox').click(function(){
@@ -26,63 +27,37 @@ $(function(){
         }else{
             $(this).parent().parent('.cart-item').removeClass('check-cart-item')
         }
+        getSum();
     })
     // 商品数量的增加与单价的增加
     $('.increment').click(function(){
         var n=$(this).siblings('.itxt').val();
         if(n<max_num){
-    // 点击增加按钮
-    $('.increment').click(function() {
-        // 增加商品数量
-        var n = $(this).siblings('.itxt').val();
-        n++;
-        if (n > 200) {
-            alert('商品不能大于200')
-        } else {
-
-            $(this).siblings('.itxt').val(n);
-        }
-        // 商品小计
-        // console.log($('.p-price').html().substr(1));
-        // 字符串. substr(2)
-        var price = ($(this).parents('.p-num').siblings('.p-price').html().substr(1) * n).toFixed(2);
-        $(this).parents('.p-num').siblings('.p-sum').html('￥' + price);
-
-    })
-    // 商品数量的增加与单价的增加
-    $('.increment').click(function(){
-        var n=$(this).siblings('.itxt').val();
-        if(n<10){
             n++;
             $(this).siblings('.itxt').val(n);
             var p=$(this).parent().parent().siblings('.p-price').html().substr(1);
             var sum=(p*n).toFixed(2);
-            $(this).parent().parent().siblings('.p-sum').html('￥'+sum);    
+            $(this).parent().parent().siblings('.p-sum').html('￥'+sum);  
+            getSum();  
         }else{
-            // alert('商品数量已达上限!');
-            // return true;
-            alert('商品数量已达上线!');
-            $(this).siblings('.itxt').val(1);
-            var p=$(this).parent().parent().siblings('.p-price').html().substr(1);
-                var sum=(p*n).toFixed();
-            $(this).parent().parent().siblings('.p-sum').html('￥'+p);
+            alert('商品数量已达上限!');
+            return true;
         }
     })
     // 商品数量的减少与单价的减少
     $('.decrement').click(function(){
         var m=$(this).siblings('.itxt').val();
         if(m>min_num){
-        if(m>1){
             m--;
             $(this).siblings('.itxt').val(m);
             var p=$(this).parent().parent().siblings('.p-price').html().substr(1);
             var sum=(p*m).toFixed(2);
             $(this).parent().parent().siblings('.p-sum').html('￥'+sum);
+            getSum();
         }else{
             alert('最少购买'+min_num+'件哦!');
             return false;
         }
-    }
     })
     // 文本框输入数值 小计随之改变 方法1
     $('.itxt').change(function(){
@@ -91,6 +66,7 @@ $(function(){
         var sum=(p*n).toFixed(2);   
         $(this).parent().parent().siblings('.p-sum').html('￥'+sum); 
         $(this).siblings('.itxt').val(n);
+        getSum();
         if(n<=min_num&&n>max_num){
             var p=$(this).parent().parent().siblings('.p-price').html().substr(1);
             var sum=(p*n).toFixed(2);   
@@ -101,7 +77,7 @@ $(function(){
             var p=$(this).parent().parent().siblings('.p-price').html().substr(1);
             var sum=(p*n).toFixed(2); 
             $(this).parent().parent().siblings('.p-sum').html('￥'+p*min_num);   
-            alert('最少购买'+min_num+'件哦!');
+            alert('最少购买一件哦!');
         }
         if(n>max_num){
             var n=$(this).val(max_num);
@@ -113,11 +89,6 @@ $(function(){
         // $(this).parent().parent().siblings('.p-sum').html('￥'+sum);    
     })
     // 方法2
-            alert('最少购买一件哦!');
-            return false;
-        }
-    })
-    // 数量模块的增减的同时改变价格小计
     // $('.itxt').change(function(){
     //     var n=$(this).val();
     //     var p=$(this).parent().parent().siblings('.p-price').html().substr(1);
@@ -125,20 +96,6 @@ $(function(){
     //     $(this).parent().parent().siblings('.p-sum').html('￥'+sum);
     // })
     // 计算商品总价值
-    
-    // getSum();
-    // function getSum(){
-    //     var count=0;
-    //     var price=0;
-    //     $('.j-checkbox:checked').parent
-        // $('.itxt').each(function(index,element){
-        //     count+=parseInt($(element).val());
-        // })
-        // $('.amount-sum em').text(count);
-        // $('.p-sum').each(function(i,ele){
-        //     price+=parseFloat($(ele).text().substr(1));
-        // })
-        // $('.price-sum em').text('￥'+ price.toFixed(2));
     getSum();
     function getSum(){
         var count=0;
