2ee4f44d807260a9257906ed9156d0f3a174cbe3
mason369 <maosenyang00@gmail.com>
2022-10-24 14:55:34 +0800

    update vue files

--- Vue.js_technical_framework/Vue3_notebook/day 01/code/code1/03.按需导出.js
+++
@@ -1,7 +0,0 @@
-export let s1 = 'aaa'
-export let s2 = 'ccc'
-export function say() {}
-
-export default {
-  a: 20
-}
--- Vue.js_technical_framework/Vue3_notebook/day 04/code/code1/src/components/02.privateReg/Search.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <p>Search 搜索组件</p>
-</template>
-
-<script>
-export default {
-  name: 'MySearch',
-}
-</script>
