9dbfbd545bf55cb9616315145672d77f9c8168c4
mason369 <1960638223@qq.com>
2022-12-20 14:09:05 +0800

    Update 书写web服务器.js
    Co-Authored-By: liuliu66 <101164913+liusxs@users.noreply.github.com>

--- Node.js_develop/node-叮叮/node04/书写web服务器.js
+++ Node.js_develop/node-叮叮/node04/书写web服务器.js
@@ -1,37 +1,35 @@
 //引入模块
-const http=require('http');
-const fs=require('fs');
-const path=require('path');
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 //定义端口号
-const port=8080;
+const port = 8080;
 //创建服务器对象
-const server= http.createServer((request,response)=>{
-    //每收到一次请求就会执行一次
-    //判断浏览器的请求，需要什么文件
-    let reqUrl=request.url;
-    console.log(reqUrl);
-    if(reqUrl=='/' || reqUrl =='/index.html'){
-        let filePath=path.join(__dirname,'assets','html','index.html');
-        let content=fs.readFileSync(filePath);
-        response.end(content)
-    }else if(reqUrl =='/cate.html'){
-        let filePath=path.join(__dirname,'assets','html','cate.html');
-        let content=fs.readFileSync(filePath);
-        response.end(content)
-    }else if(reqUrl.endsWith('.css')){
-        let filePath=path.join(__dirname,'assets','css','index.css');
-        let content=fs.readFileSync(filePath);
-        response.end(content)
-    }else{
-        response.end('404，报错了')
-    }
-
-    
-})
+const server = http.createServer((request, response) => {
+	//每收到一次请求就会执行一次
+	//判断浏览器的请求，需要什么文件
+	let reqUrl = request.url;
+	console.log(reqUrl);
+	if (reqUrl == "/" || reqUrl == "/index.html") {
+		let filePath = path.join(__dirname, "assets", "html", "index.html");
+		let content = fs.readFileSync(filePath);
+		response.end(content);
+	} else if (reqUrl == "/cate.html") {
+		let filePath = path.join(__dirname, "assets", "html", "cate.html");
+		let content = fs.readFileSync(filePath);
+		response.end(content);
+	} else if (reqUrl.endsWith(".css")) {
+		let filePath = path.join(__dirname, "assets", "css", "index.css");
+		let content = fs.readFileSync(filePath);
+		response.end(content);
+	} else {
+		response.end("404，报错了");
+	}
+});
 //服务器监听
-server.listen(port,(error)=>{
-    console.log('服务器运行成功');
-})
+server.listen(port, (error) => {
+	console.log("服务器运行成功");
+});
 
 //第三方模块，进行安装
 //npm init项目初始化
