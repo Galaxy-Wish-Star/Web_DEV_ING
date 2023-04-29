1c13c0fca4ec66178d750474b04c0b6fddaebbb4
mason369 <1960638223@qq.com>
2023-04-29 14:41:01 +0800

    mods

--- mobile_Dev/js/shopCar.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-	let back = document.getElementsByClassName('header-left')
-	back[0].addEventListener('click', function () {
-		window.history.back()
-	})
-})
